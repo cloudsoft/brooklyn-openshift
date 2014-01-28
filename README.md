f10d5e27ea99dd5b29626398bbcab04b497fe5e1
Aled Sage <aled.sage@gmail.com>
2014-01-28 10:35:42 +0000

    Add code, moved from core brooklyn

---
+++ README.md
@@ -0,0 +1,11 @@
+Brooklyn Integration for OpenShift PaaS System
+==============================================
+
+This project provides [Brooklyn](http://brooklyncentral.github.io/) entities for the [OpenShift](https://www.openshift.com/) PaaS System.
+
+================
+
+&copy; Cloudsoft Corporation 2013. All rights reserved.
+
+Use of this software is subject to the Cloudsoft EULA, provided in LICENSE.txt and at http://www.cloudsoftcorp.com/cloudsoft-developer-license
+
---
+++ openshift-paas/src/test/java/io/cloudsoft/systems/openshift/paas/LocationResolvesTest.java
@@ -0,0 +1,29 @@
+package io.cloudsoft.systems.openshift.paas;
+import org.testng.Assert;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+import brooklyn.location.LocationRegistry;
+import brooklyn.management.internal.LocalManagementContext;
+
+public class LocationResolvesTest {
+
+    private LocalManagementContext managementContext;
+
+    @BeforeMethod(alwaysRun=true)
+    public void setUp() throws Exception {
+        managementContext = new LocalManagementContext();
+    }
+    
+    @AfterMethod(alwaysRun=true)
+    public void tearDown() throws Exception {
+        if (managementContext != null) managementContext.terminate();
+    }
+    
+    @Test
+    public void testOpenshiftLocationResolves() {
+        LocationRegistry reg = managementContext.getLocationRegistry();
+        Assert.assertNotNull(reg.resolve("openshift"));
+    }
+}
